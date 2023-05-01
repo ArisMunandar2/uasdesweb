22eff5d998c9de81a5064a58436b202539bb06ba
ArisMunandar2 <124014684+ArisMunandar2@users.noreply.github.com>
2023-05-02 00:31:16 +0700

    Add files via upload

---
+++ asset/datamusic.js
@@ -0,0 +1,19 @@
+let artikel1 = [
+    "Suga BTS : 'Agust D akan Segera Comeback'",
+    "asset/img/foto header.jpeg",
+    "<div class='isi'><p> Suga BTS aka Agust D akan comeback pada bulan ini! Dilansir dari Weverse News, Suga akan melangsungkan comeback dengan menggarap lagu baru dan kolaborasi dengan berbagai penyanyi lainnya. Album solonya secara resmi akan dirilis pada 21 April 2023 dengan nama &ldquo;D-DAY&rdquo; yang merupakan bagian terakhir dari Trilogy Agust D. Sebelumnya, penyanyi rapp ini sempat mengumunkan bahwa dirinya bakal merilis film dokumenter solo bertajuk ‘Suga : Road to D-DAY’ pada akhir April mendatang.Dalam film tersebut akan menjelaskan tentang bagaimana Suga memproses pembuatan album tersebut. Suga menulis dan menyusun semua lagu yang ada dalam album D-DAY, dan bertangghung jawab atas keseluruhan proses produksi untuk membagikan kisah pribadinya yang tulus. Sebelumnya Suga telah merilis lagu pra-rilis dari album D-DAY, yaitu lagu ‘People Pt.2’ yang berkolaborasi dengan penyanyi wanita IU, yang telah dirilis pada Jumat kemarin, 7 April 2023.</p></div><div class='isi'><p> Selain itu, HYBE juga sudah merilis PROMOTION SCHEDULE untuk jadwal comebacknya. Mulai dari D-DAY Tracklist, Glitch Film, Concept Photo 1 & 2, hingga perilisan MV Official. Selain itu, perilisan Tracklist membuat penggemar sudah tidak sabar untuk menanti lagu buatannya. Berbagai kolaborasi dalam pembuatan lagunya membuat ARMY (fans) sangat antusias untuk mendengarkan lagu tersebut, dimulai kolaborasinya dengan IU, RM & Jhope, hingga produser musik asal Jepang Ryuichi Sakamoto.  </p></div>"
+];
+
+let artikel2 = [
+    "SUGA'S Musical Journey",
+    "asset/img/judul 1.jpg",
+    "Suga BTS telah bersiap untuk merilis album debut solonya yang akan datang D-DAY, telah berbicara tentang perjalanan musiknya. Dalam wawancaranya dengan Billboard, bintang K-pop ini mengungkapkan bahwa &ldquo;ketika kami pertama kali memulai debut, kami berada dalam posisi ambigu untuk tidak diterima sebagai musisi dan juga tidak diterima sebagai idola&rdquo;. Baru-baru ini, BIGHIT MUISC merilis pernyataan resmi terkait perilisan album solo Suga dan mengatakan 'D-DAY' menandai bab penutup dari trilogi oleh julukan lain SUGA, Agust D, menggantikan mixtape sebelumnya, 'Agust D' dan 'H-2.' Album ini menggali perjalanan pribadi SUGA sebagai Agust D, menawarkan gambaran intim tentang hidupnya sebagai seorang seniman. Kami meminta cinta dan dukungan penggemar untuk bab terakhir dari triloginya.Sementara itu, D-Day akan dirilis pada 21 April 2023."
+];
+
+let artikel3 = [
+    'Kolaborasi IU & SUGA Dalam Album "D-DAY"',
+    "asset/img/judul 2.png",
+    "Suga BTS belum lama ini mengumumkan akan merilis album solo resmi bertajuk D-DAY. Jelang perilisan, ia akan mengeluarkan single pra-rilis yang berkolaborasi dengan salah satu musisi papan atas Korea Selatan. Suga BTS belum lama ini mengumumkan akan merilis album solo resmi bertajuk D-DAY. Jelang perilisan, ia akan mengeluarkan single pra-rilis yang berkolaborasi dengan salah satu musisi papan atas Korea Selatan. Lagu ini akan menjadi pembuka dari rangkaian debut solo resmi Suga lewat album D-DAY. People Pt.2 bisa mulai didengarkan di berbagai situs streaming pada 7 April 2023 pukul 11.00 WIB. Dikutip dari Naver, People Pt2. (feat IU) dideskripsikan sebagai lagu yang mengurangi hubungan antara sesama manusia yang terus berulang, juga emosi yang dihasilkan dari hubungan tersebut. Lagu ini menggunakan ritme boom bap yang diselaraskan dengan rap Agust D dan suara IU yang membius."
+];
+
+export {artikel1,artikel2,artikel3}
